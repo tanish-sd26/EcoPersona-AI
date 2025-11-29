b80f2cd95054fc429a0dfdd033df6dfab9cba487
tanish-sd26 <mauryatanisha824@gmail.com>
2025-11-29 14:06:34 +0530

    correction in file import

--- backend/src/routes/quizRoutes.js
+++ backend/src/routes/quizRoutes.js
@@ -1,5 +1,5 @@
 import express from "express";
-import {submitQuiz  } from "../controllers/personaControllers.js";
+import { submitQuiz } from "../controllers/quizControllers.js";
 const router = express.Router();
 
 router.post("/submit", submitQuiz);
