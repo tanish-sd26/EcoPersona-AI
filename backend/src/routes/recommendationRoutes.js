2ccf25bf31ed71b8feee4ef7d15fa2c53903364b
tanish-sd26 <mauryatanisha824@gmail.com>
2025-11-29 14:19:27 +0530

    remove temporary routes

--- backend/src/routes/recommendationRoutes.js
+++ backend/src/routes/recommendationRoutes.js
@@ -1,9 +1,6 @@
 import express from "express";
 const router = express.Router();
 
-// Temporary test route
-router.get("/", (req, res) => {
-  res.json({ message: "Recommendation route working" });
-});
+
 
 export default router;
