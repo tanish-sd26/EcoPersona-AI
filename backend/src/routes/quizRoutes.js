5d0fcb2a46035299188b6132c8a9c16de71bee75
tanish-sd26 <mauryatanisha824@gmail.com>
2025-11-29 13:55:54 +0530

    remove temporary route

--- backend/src/routes/quizRoutes.js
+++ backend/src/routes/quizRoutes.js
@@ -1,9 +1,6 @@
 import express from "express";
 const router = express.Router();
 
-// Temporary test route
-router.get("/", (req, res) => {
-  res.json({ message: "quiz route working" });
-});
+
 
 export default router;
