65aec190e8a3c3fc58118ce49185a1d0c043629f
tanish-sd26 <mauryatanisha824@gmail.com>
2025-11-29 13:58:15 +0530

    import personacontroller file and update routes

--- backend/src/routes/quizRoutes.js
+++ backend/src/routes/quizRoutes.js
@@ -1,6 +1,7 @@
 import express from "express";
+import {submitQuiz  } from "../controllers/personaControllers.js";
 const router = express.Router();
 
-
+router.post("/submit", submitQuiz);
 
 export default router;
