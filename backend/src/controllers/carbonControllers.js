6ddbdea9e178c2a3a2e9cd8a9ba62bf7afb06ce6
tanish-sd26 <mauryatanisha824@gmail.com>
2025-11-29 16:08:27 +0530

    updated carbonControllers.js

--- backend/src/controllers/carbonControllers.js
+++ backend/src/controllers/carbonControllers.js
@@ -1,1 +1,13 @@
 import { calculateCarbonFootprint } from "../ai/carbonFormula.js";
+export const estimateCarbon = (req, res) => {
+  try {
+    const data = req.body; // electricity, transport, diet, water, waste
+    const result = calculateCarbonFootprint(data);
+    res.status(200).json({
+      success: true,
+      carbonFootprint: result,
+    });
+  } catch (error) {
+    res.status(500).json({ success: false, message: "Error calculating carbon footprint" });
+  }
+};
