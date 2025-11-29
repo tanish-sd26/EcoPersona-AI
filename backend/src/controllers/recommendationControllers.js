c8a38be637b6a53911a6eae2c9789cde8de13c84
tanish-sd26 <mauryatanisha824@gmail.com>
2025-11-29 14:17:20 +0530

    update recommendationControlers.js

--- backend/src/controllers/recommendationControllers.js
+++ backend/src/controllers/recommendationControllers.js
@@ -1,1 +1,15 @@
+import { generateRecommendations } from "../ai/recommendationEngine.js";
+
+export const getRecommendations = async (req, res) => {
+  try {
+    const { persona, score } = req.body;
+
+    if (!persona || score === undefined) {
+      return res.status(400).json({
+        message: "Persona or score missing",
+      });
+    }
+
+ 
+};
 
