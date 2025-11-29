e377262059683decc7ac4211b2ac896e52fc7c82
tanish-sd26 <mauryatanisha824@gmail.com>
2025-11-29 13:07:15 +0530

    signup updates at control folder

--- backend/src/controllers/authControllers.js
+++ backend/src/controllers/authControllers.js
@@ -1,26 +1,27 @@
 import User from "../models/User.js";
-import generateToken from "../utils/generateTokens.js";
+import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
 
-// Register User
-export const registerUser = async (req, res) => {
-  const { name, email, password } = req.body;
+export const signup = async (req, res) => {
   try {
-    const userExists = await User.findOne({ email });
-    if (userExists) return res.status(400).json({ message: "User already exists" });
+    const { name, email, password } = req.body;
 
-    const user = await User.create({ name, email, password });
-    if (user) {
-      res.status(201).json({
-        _id: user._id,
-        name: user.name,
-        email: user.email,
-        token: generateToken(user._id),
-      });
-    } else {
-      res.status(400).json({ message: "Invalid user data" });
+    const existingUser = await User.findOne({ email });
+    if (existingUser) {
+      return res.status(400).json({ message: "Email already registered" });
     }
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    const newUser = await User.create({
+      name,
+      email,
+      password: hashedPassword,
+    });
+
+    res.status(201).json({ message: "Signup successful", userId: newUser._id });
   } catch (error) {
-    res.status(500).json({ message: error.message });
+    res.status(500).json({ message: "Server error", error });
   }
 };
 
