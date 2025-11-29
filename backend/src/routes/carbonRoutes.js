e81ccdec9a9c33c3bc2dedcba7e4dd702e864016
tanish-sd26 <mauryatanisha824@gmail.com>
2025-11-29 16:11:13 +0530

    add route to carbonRoutes.js

--- backend/src/routes/carbonRoutes.js
+++ backend/src/routes/carbonRoutes.js
@@ -1,6 +1,7 @@
 import express from "express";
+import { estimateCarbon } from "../controllers/carbonControllers.js";
 const router = express.Router();
 
-
+router.post("/", estimateCarbon);
 
 export default router;
