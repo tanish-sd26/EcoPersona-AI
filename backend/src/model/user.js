ce66cc2977b4ccbd92069e17d91974c1a2b76203
tanish-sd26 <mauryatanisha824@gmail.com>
2025-11-29 13:02:02 +0530

    added personaType, quizScore, timestamps, structure lines to user.js

--- backend/src/model/user.js
+++ backend/src/model/user.js
@@ -1,42 +1,37 @@
 import mongoose from "mongoose";
-import bcrypt from "bcryptjs";
+import bcrypt from "bcryptjs";  
 
-const userSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: true,
-    trim: true,
-  },
-  email: {
-    type: String,
-    required: true,
-    unique: true,
-    lowercase: true,
-    trim: true,
-  },
-  password: {
-    type: String,
-    required: true,
-    minlength: 6,
-  },
-  createdAt: {
-    type: Date,
-    default: Date.now,
-  },
-});
+const userSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
 
-// Password hash before save
-userSchema.pre("save", async function (next) {
-  if (!this.isModified("password")) return next();
-  const salt = await bcrypt.genSalt(10);
-  this.password = await bcrypt.hash(this.password, salt);
-  next();
-});
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
 
-// Method to compare password
-userSchema.methods.matchPassword = async function (enteredPassword) {
-  return await bcrypt.compare(enteredPassword, this.password);
-};
+    password: {
+      type: String,
+      required: true,
+      minlength: 6,
+    },
 
-const User = mongoose.model("User", userSchema);
-export default User;
+    personaType: {
+      type: String,
+      default: null, 
+    },
+
+    quizScores: {
+      type: Object,
+      default: {},
+    },
+  },
+  { timestamps: true }
+);
+
+export default mongoose.model("User", userSchema);
+
