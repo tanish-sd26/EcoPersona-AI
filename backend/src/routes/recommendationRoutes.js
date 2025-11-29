3aa465bcbe3dde5eccc6a3687b527e22c1fbda63
tanish-sd26 <mauryatanisha824@gmail.com>
2025-11-29 14:20:38 +0530

    import and create route

--- backend/src/routes/recommendationRoutes.js
+++ backend/src/routes/recommendationRoutes.js
@@ -1,6 +1,7 @@
 import express from "express";
+import { getRecommendations } from "../controllers/recommendationControllers.js";
 const router = express.Router();
 
-
+router.post("/generate", getRecommendations);
 
 export default router;
